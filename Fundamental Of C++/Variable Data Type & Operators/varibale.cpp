0f4aaed038e53a158f5a245b5e68d44b66e18fd9
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-11-02 08:23:42 +0600

    Variable: container to store data

---
+++ Fundamental Of C++/Variable Data Type & Operators/varibale.cpp
@@ -0,0 +1,16 @@
+
+
+// Variable - container to store data
+// starts with letter or _ 
+// cannot start with number
+// cannot contain space
+// case sensitive
+
+#include<iostream>
+using namespace std;
+int main()
+{
+     a = 10; // !!!always we have to use a data type before define a variable here it is int 
+     _b = 'a';
+   
+}
