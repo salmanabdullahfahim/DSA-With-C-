eed5f991fda10b3a10681099ee6f3d3656bdd97c
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-11-03 20:37:07 +0600

    Arithmetic operator

---
+++ Fundamental Of C++/Variable Data Type & Operators/operators.cpp
@@ -0,0 +1,18 @@
+// Operators
+// 1. Arithmetic - +, -, *, /, %, ++, --
+
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int a = 10, b = 5;
+
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+    cout << a++ << endl;
+    cout << a-- << endl;
+}
