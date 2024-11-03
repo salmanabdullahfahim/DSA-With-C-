59dcad84ddc55da78a1562a3472171b4f1f4a328
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-11-03 21:20:26 +0600

    ⚡Simple problem- Sum of two numbers

---
+++ Fundamental Of C++/Variable Data Type & Operators/sumOfTwoNumber.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter first number: ";
+    cin>>a;
+    cout<<"Enter second number: ";
+    cin>>b;
+    cout<<"Sum of two number is: "<<a+b<<endl;
+    return 0;
+}
